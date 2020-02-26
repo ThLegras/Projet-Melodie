5df1d7d61e2c6543f71f03c982f509d6ed3fc583
ThLegras <38461332+ThLegras@users.noreply.github.com>
2020-02-26 14:22:37 +0100

    horolge + update de time

---
+++ Assets/Clock/Scripts/Clock.cs
@@ -0,0 +1,102 @@
+using UnityEngine;
+using System.Collections;
+using Valve.VR;
+
+public class Clock : MonoBehaviour {
+//-----------------------------------------------------------------------------------------------------------------------------------------
+//-----------------------------------------------------------------------------------------------------------------------------------------
+//-----------------------------------------------------------------------------------------------------------------------------------------
+//
+//  Simple Clock Script / Andre "AEG" Bürger / VIS-Games 2012
+//
+//-----------------------------------------------------------------------------------------------------------------------------------------
+//-----------------------------------------------------------------------------------------------------------------------------------------
+//-----------------------------------------------------------------------------------------------------------------------------------------
+
+    //-- set start time 00:00
+    public int minutes = 0;
+    public int hour = 0;
+    
+    //-- time speed factor
+    public float clockSpeed = 1.0f;     // 1.0f = realtime, < 1.0f = slower, > 1.0f = faster
+
+    //-- internal vars
+    int seconds;
+    float msecs;
+    GameObject pointerSeconds;
+    GameObject pointerMinutes;
+    GameObject pointerHours;
+    float LastTime;
+    public Vector2 previous_pos;//acienne position du doigt sur le touchpad
+    float number;
+    float tempsMesure = 0.05f;
+    //-----------------------------------------------------------------------------------------------------------------------------------------
+    //-----------------------------------------------------------------------------------------------------------------------------------------
+    //-----------------------------------------------------------------------------------------------------------------------------------------
+    void Start() 
+    {
+        pointerMinutes = transform.Find("rotation_axis_pointer_minutes").gameObject;
+
+        msecs = 0.0f;
+        seconds = 0;
+        LastTime = Time.time;
+        previous_pos = new Vector2(1.0f, 0.0f);//position initiale de notre doigt
+        number = 0.0f;
+    }
+
+
+    public SteamVR_Action_Vector2 touchPadAction;
+
+    void Update()
+    {
+
+        /*Vector2 touchPadValue = touchPadAction.GetAxis(SteamVR_Input_Sources.Any);
+
+        if (LastTime - Time.time < 0.1f)
+        {
+            if (touchPadValue != Vector2.zero)
+            {
+                if (((touchPadValue[0] - previous_pos[0]) < 0 && touchPadValue[1] > 0) || ((touchPadValue[0] - previous_pos[0]) > 0 && touchPadValue[1] < 0))
+                {
+                    clockSpeed -= 0.005f;
+                    previous_pos = touchPadValue;
+                }
+                if (((touchPadValue[0] - previous_pos[0]) > 0 && touchPadValue[1] > 0) || ((touchPadValue[0] - previous_pos[0]) < 0 && touchPadValue[1] < 0))
+                {
+                    clockSpeed += 0.005f;
+                    previous_pos = touchPadValue;
+                }
+            }
+        }*/
+        number = Input.GetAxis("Mouse ScrollWheel");
+        clockSpeed += number;
+        //-- calculate time
+        msecs += Time.deltaTime * clockSpeed;
+        if(msecs >= tempsMesure)
+        {
+            msecs -= tempsMesure;
+            seconds++;
+            if(seconds >= 60)
+            {
+                seconds = 0;
+            }
+        }else if (msecs <= -tempsMesure)
+        {
+            msecs += tempsMesure;
+            seconds--;
+            if (seconds <= 0)
+            {
+                seconds = 60;
+            }
+        }
+
+
+        //-- calculate pointer angles
+        float rotationMinutes = (360.0f / 60.0f)  * seconds;
+
+        //-- draw pointers
+        pointerMinutes.transform.localEulerAngles = new Vector3(0.0f, 0.0f, rotationMinutes);
+
+    }
+
+}
--- Assets/Scripts/Bulles/Instrument.cs
+++ Assets/Scripts/Bulles/Instrument.cs
@@ -8,44 +8,18 @@ public class Instrument : MonoBehaviour
     public float time;
     public int volume;//puissance sonore de la source du son en mW
     public int notes; //nombre de notes jouables sur l'instrument
-    public Vector2 previous_pos;//acienne position du doigt sur le touchpad
-    float LastTime;
-    float number;
     // Start is called before the first frame update
     void Start()
     {
         time = 1;
-        number = 0.0f;
-        previous_pos = new Vector2(1.0f, 0.0f);//position initiale de notre doigt
         volume = 0;
-        LastTime = Time.time;
     }
-
-    public SteamVR_Action_Vector2 touchPadAction;
+    
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 touchPadValue = touchPadAction.GetAxis(SteamVR_Input_Sources.Any);
-
-        if(LastTime - Time.time < 0.1f)
-        {
-            if (touchPadValue != Vector2.zero)
-            {
-                if (((touchPadValue[0] - previous_pos[0]) < 0 && touchPadValue[1] > 0) || ((touchPadValue[0] - previous_pos[0]) > 0 && touchPadValue[1] < 0))
-                {
-                    time -= 0.005f;
-                    previous_pos = touchPadValue;
-                }
-                if (((touchPadValue[0] - previous_pos[0]) > 0 && touchPadValue[1] > 0) || ((touchPadValue[0] - previous_pos[0]) < 0 && touchPadValue[1] < 0))
-                {
-                    time += 0.005f;
-                    previous_pos = touchPadValue;
-                }
-                Debug.Log(touchPadValue);
-            }
-        }
-        number = Input.GetAxis("Mouse ScrollWheel");
-        time += number;
+        Debug.Log(time);
+        time = GameObject.Find("clock").transform.GetComponent<Clock>().clockSpeed;
     }
 }
