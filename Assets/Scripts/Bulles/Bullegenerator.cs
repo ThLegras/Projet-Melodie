0723ccd95aa057d32b597f075a3b8f053b3800f7
Jordane Minet <jordane.minetclaret@gmail.com>
2020-02-26 01:12:35 +0100

    [ADD] Replay sounds
    Modified bubblegenerator script and way to create bubble

--- Assets/Scripts/Bulles/Bullegenerator.cs
+++ Assets/Scripts/Bulles/Bullegenerator.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Bullegenerator : MonoBehaviour
 {
+    public static List<SaveBubble> allBubblesCreated = new List<SaveBubble>();
+    public static bool saveBubblesBool = true;
+
     private List<GameObject> objects;
     new Renderer renderer;
     Color temp;
@@ -13,6 +16,7 @@ public class Bullegenerator : MonoBehaviour
     float m_Hue;
     float m_Saturation;
     float m_Value;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,12 +24,10 @@ public class Bullegenerator : MonoBehaviour
         renderer = GetComponent<Renderer>();
         temp = renderer.material.color;
         m_Hue = (float)note / transform.root.GetComponent<Instrument>().notes;
-        Debug.Log(m_Hue);
+        //Debug.Log(m_Hue);
         m_Saturation = 0.8f;
         m_Value = 0.8f;
     }
-
-
 
     // Update is called once per frame
     void Update()
@@ -33,27 +35,41 @@ public class Bullegenerator : MonoBehaviour
         
     }
 
+    public static void CreateBubble(SaveBubble save)
+    {
+        GameObject sphere = GameObject.Instantiate(GameObject.Find("BubbleExample"), save.sourceTransform);
+        sphere.AddComponent<Bubble>();
+        sphere.transform.SetParent(save.sourceTransform.root.transform);//on désigne le cube comme parent de l'objet
+        sphere.transform.GetComponent<Renderer>().material.color = save.color;
+        sphere.gameObject.GetComponent<Bubble>().myAudioSource = save.audio;
+        sphere.gameObject.GetComponent<Bubble>().note = save.note;
+        sphere.gameObject.GetComponent<Bubble>().son = save.son;
+        sphere.transform.position = save.sourceTransform.position;
+        //objects.Add(sphere);
+
+
+        GameObject invSphere = GameObject.Instantiate(GameObject.Find("BubbleExampleReverse"), save.sourceTransform);
+        invSphere.AddComponent<ReversedBubble>();
+        invSphere.transform.SetParent(save.sourceTransform.root.transform);//on désigne le cube comme parent de l'objet
+        invSphere.transform.GetComponent<Renderer>().material.color = save.color;
+        invSphere.transform.position = save.sourceTransform.position;
+        //objects.Add(invSphere);
+    }
+
     private void OnMouseDown()
     {
-        GameObject sphere = GameObject.Instantiate(GameObject.Find("BubbleExample"), transform);
-        sphere.AddComponent<Bubble>();
-        sphere.transform.SetParent(transform.root.transform);//on désigne le cube comme parent de l'objet
-        sphere.transform.GetComponent<Renderer>().material.color = Color.HSVToRGB(m_Hue, m_Saturation, m_Value);
-        sphere.gameObject.GetComponent<Bubble>().myAudioSource = GetComponent<AudioSource>();
-        sphere.gameObject.GetComponent<Bubble>().note = note;
-        sphere.gameObject.GetComponent<Bubble>().son = son;
-        sphere.transform.position = transform.position;
-        objects.Add(sphere);
+        SaveBubble save = new SaveBubble(transform, Color.HSVToRGB(m_Hue, m_Saturation, m_Value), GetComponent<AudioSource>(), note, son, Time.time);
+        if (Bullegenerator.saveBubblesBool)
+            Bullegenerator.allBubblesCreated.Add(save);
+        Bullegenerator.CreateBubble(save);
+    }
 
-
-
-
-        GameObject invSphere = GameObject.Instantiate(GameObject.Find("BubbleExampleReverse"), transform);
-        invSphere.AddComponent<ReversedBubble>();
-        invSphere.transform.SetParent(transform.root.transform);//on désigne le cube comme parent de l'objet
-        invSphere.transform.GetComponent<Renderer>().material.color = Color.HSVToRGB(m_Hue, m_Saturation, m_Value);
-        invSphere.transform.position = transform.position;
-        objects.Add(invSphere);
+    private void OnTriggerEnter(Collider other)
+    {
+        SaveBubble save = new SaveBubble(transform, Color.HSVToRGB(m_Hue, m_Saturation, m_Value), GetComponent<AudioSource>(), note, son, Time.time);
+        if (Bullegenerator.saveBubblesBool)
+            Bullegenerator.allBubblesCreated.Add(save);
+        Bullegenerator.CreateBubble(save);
     }
 
     /*private void OnHandOverBegin()
@@ -72,7 +88,7 @@ public class Bullegenerator : MonoBehaviour
     }*/
 
 
-        private void OnMouseEnter()
+    private void OnMouseEnter()
     {
         transform.GetComponent<Renderer>().material.color = new Color(1 - temp.r, 1 - temp.g, 1 - temp.b, -1.0f);
     }
@@ -80,5 +96,28 @@ public class Bullegenerator : MonoBehaviour
     private void OnMouseExit()
     {
         renderer.material.color = temp;
+    }
+}
+
+/**
+ * Used to save information about bubble created. Used in case of sound played history.
+ */
+public struct SaveBubble
+{
+    public Transform sourceTransform;
+    public Color color;
+    public AudioSource audio;
+    public float note;
+    public AudioClip son;
+    public float time;
+
+    public SaveBubble(Transform sourceTransform, Color color, AudioSource audio, float note, AudioClip son, float time)
+    {
+        this.sourceTransform = sourceTransform;
+        this.color = color;
+        this.audio = audio;
+        this.note = note;
+        this.son = son;
+        this.time = time;
     }
 }
---
+++ Assets/Scripts/Bulles/ReplaySounds.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static Bullegenerator;
+
+public class ReplaySounds : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        StartCoroutine(ReplayAllBubbles());
+    }
+
+    public void OnMouseDown()
+    {
+        StartCoroutine(ReplayAllBubbles());
+    }
+
+    // Play every bubble saved in allBubblesCreated
+    private IEnumerator ReplayAllBubbles()
+    {
+        Bullegenerator.saveBubblesBool = false;
+        if(Bullegenerator.allBubblesCreated.Count != 0)
+        {
+            float lastTime = Bullegenerator.allBubblesCreated[0].time;
+            foreach (SaveBubble s in Bullegenerator.allBubblesCreated)
+            {
+                yield return new WaitForSeconds(s.time - lastTime);
+                Bullegenerator.CreateBubble(s);
+                lastTime = s.time;
+            }
+            Bullegenerator.allBubblesCreated.Clear();
+        }
+        Bullegenerator.saveBubblesBool = true;
+    }
+}
